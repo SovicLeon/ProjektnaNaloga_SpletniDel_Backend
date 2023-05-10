0449c923853d5540c888eac08ae16e32577c0885
ericQronaveter <eric.mrbadass@gmail.com>
2023-05-10 20:37:21 +0200

    Added vehicle data model

---
+++ models/vehicleDataModel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var vehicleDataSchema = new Schema({
+	'longitude' : Number,
+	'latitude' : Number,
+	'gyro_x' : Number,
+    'gyro_y' : Number,
+    'gyro_z' : Number,
+    'acc_acceleration' : Number,
+    'acc_x' : Number,
+    'acc_y' : Number,
+    'acc_z' : Number,
+    'timestamp' : Date,
+});
+
+module.exports = mongoose.model('vehicleData', vehicelDataSchema);
